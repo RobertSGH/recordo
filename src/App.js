addae4d4b7b25a4e65fc673e64108f5c9d63760d
RobertSGH <robert.saban1989@gmail.com>
2023-01-05 12:19:12 +0100

    c

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Layout from './components/UI/layout/Layout';
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-// import { useContext } from 'react';
-// import AuthContext from './store/auth-context';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBNogwN8AELoHzMzvjK78eihuzOls0kx38',
@@ -23,9 +21,6 @@ export const auth = getAuth();
 export const provider = new GoogleAuthProvider();
 
 function App() {
-  // const authCtx = useContext(AuthContext);
-  // console.log(authCtx);
-
   return <Layout />;
 }
 
